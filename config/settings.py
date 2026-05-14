97789fb991ac1593d74ec1dfbf39d2b0f29dd59e
emrahustundag <ustundag902@gmail.com>
2026-05-15 01:56:55 +0300

    fix CSRF trusted origins

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = ['https://eussec.com', 'https://www.eussec.com', 'https://eussec-production.up.railway.app']
+
 
 # Application definition
 
