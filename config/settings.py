53e406e15f397c20e2e41eba1977df56cffa85ea
emrahustundag <ustundag902@gmail.com>
2026-05-18 00:29:36 +0300

    force HTTPS redirect

--- config/settings.py
+++ config/settings.py
@@ -124,3 +124,8 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ] + MIDDLEWARE[1:]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
